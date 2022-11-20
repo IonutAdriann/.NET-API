a0acb1509d5f4dfbb9c1bb2648c48f50080e8370
Ionut Adrian <61465454+IonutAdriann@users.noreply.github.com>
2022-11-20 18:55:29 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿namespace JsonIntro;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public int Age { get; set; }
+    
+    public double Height { get; set; }
+    
+    public bool IsMarried { get; set; }
+    
+    public char Sex { get; set; }
+    
+    public string[] Hobbies { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using JsonIntro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Ionut = new Person
+        {
+            FirstName = "Ionut",
+            LastName = "Rotaru",
+            Age = 21,
+            Height = 190,
+            IsMarried = false,
+            Sex = 'M',
+            Hobbies = new string[] { "Football, Basketball, Programming" }
+        };
+
+        var Alex = new Person
+        {
+            FirstName = "Alex",
+            LastName = "Ionescu",
+            Age = 21,
+            Height = 183,
+            IsMarried = false,
+            Sex = 'M',
+            Hobbies = new string[] { "Voleyball, Basketball" }
+            
+        };
+        var Bogdan = new Person
+        {
+            FirstName = "Bogdan",
+            LastName = "Drug",
+            Age = 22,
+            Height = 192,
+            IsMarried = false,
+            Sex = 'M',
+            Hobbies = new string[] { "Gaming, football" }
+            
+        };
+        var people = new List<Person> { Ionut, Alex, Bogdan };
+
+        string peopleAsJson = JsonSerializer.Serialize(people, new JsonSerializerOptions { WriteIndented = true });
+        Console.WriteLine(peopleAsJson);
+    }
+}
+
